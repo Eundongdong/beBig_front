0c72cda7951001ac89e745396b56319358e42cc3
이승아 <dltmddk981119@gmail.com>
2024-09-25 14:20:47 +0900

    [BEBIG-51] feat : 소셜로그인 화면개발, 정책 가져오기

--- src/pages/user/Login.vue
+++ src/pages/user/Login.vue
@@ -1,109 +1,169 @@
-    <template>
-        <div class="Page">
-            <div class="Logo">
-                <h1>beBIG</h1>
-            </div>
-            <div class="Input">
-                <ul>
-                    <li>
-                        <h3>아이디</h3>
-                        <input type="text" v-model="loginUser.userId" placeholder="Enter your ID"/>
-                    </li>
-                    <li>
-                        <h3>비밀번호</h3>
-                        <input type="password" v-model="loginUser.password" placeholder="Enter your password"/>
-                    </li>
-                    <li>
-                        <button class="button" @click="GoSignup">회원가입</button>
-                        <button class="button" :disabled="disableSubmit" @click="login">로그인</button>
-                    </li>
-                    <li>
-                        <button class="button" @click="GoFindId">아이디 찾기</button>
-                        <button class="button" @click="GoFindPwd">비밀번호 찾기</button>
-                    </li>
-                </ul>
-            </div>
-            <div class="Button">
-                <ul>
-                    <li><button class="naver_button" @click="naverSocialLogin">네이버 로그인</button></li>
-                    <li><button class="kakao_button" @click="naverSocialLogin">네이버 로그인</button></li>
-                    <li><button class="unsign_button" @click="unsignLogin">가입없이 이용</button></li>
-                </ul>
-            </div>
-        </div>
+<template>
+  <div class="Page">
+    <div class="Logo">
+      <h1>beBIG</h1>
+    </div>
+    <div class="Input">
+      <ul>
+        <li>
+          <h3>아이디</h3>
+          <input
+            type="text"
+            v-model="loginUser.userId"
+            placeholder="Enter your ID"
+          />
+        </li>
+        <li>
+          <h3>비밀번호</h3>
+          <input
+            type="password"
+            v-model="loginUser.password"
+            placeholder="Enter your password"
+          />
+        </li>
+        <li>
+          <button
+            class="button"
+            @click="GoSignup"
+          >
+            회원가입
+          </button>
+          <button
+            class="button"
+            :disabled="disableSubmit"
+            @click="login"
+          >
+            로그인
+          </button>
+          <button
+            class="button"
+            @click="GoSocialSignup"
+          >
+            소셜회원가입
+          </button>
+        </li>
+        <li>
+          <button
+            class="button"
+            @click="GoFindId"
+          >
+            아이디 찾기
+          </button>
+          <button
+            class="button"
+            @click="GoFindPwd"
+          >
+            비밀번호 찾기
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div class="Button">
+      <ul>
+        <li>
+          <button
+            class="naver_button"
+            @click="naverSocialLogin"
+          >
+            네이버 로그인
+          </button>
+        </li>
+        <li>
+          <button
+            class="kakao_button"
+            @click="naverSocialLogin"
+          >
+            카카오 로그인
+          </button>
+        </li>
+        <li>
+          <button
+            class="unsign_button"
+            @click="unsignLogin"
+          >
+            가입없이 이용
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
 
-    </template>
-    
-    <script setup>
-    import { ref, reactive, computed } from 'vue';
-    import { useRouter} from 'vue-router';
-    import {useUserStore} from '@/stores/user';
-    
-    //////변수 선언
-    const router = useRouter();
-    const userStore = useUserStore();
+<script setup>
+import { ref, reactive, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useUserStore } from '@/stores/user';
 
-    const loginUser = reactive({
-        userId: '',
-        password: '',
-    });
+//////변수 선언
+const router = useRouter();
+const userStore = useUserStore();
 
-    const noLogin = reactive({
-        userId: 'test',
-        password: '0000',
-    });
-    
-    // id, password 입력 확인 => id, password 입력시 버튼 활성화
-    const disableSubmit = computed(() => !(loginUser.userId && loginUser.password));
+const loginUser = reactive({
+  userId: '',
+  password: '',
+});
 
-    //로그인
-    const login = async() =>{
-        console.log(loginUser);
-        try{
-            await userStore.login(loginUser);
-            router.push('/home')
-        }catch(error){
-            console.log('에러 =>', error.message);  // 에러 로그 출력
-            alert(error.message);  // 경고창으로 에러 메시지 표시
-        }
-    };
+const noLogin = reactive({
+  userId: 'test',
+  password: '0000',
+});
 
-    //가입없이 이용
-    const unsignLogin = async() =>{
-        console.log(loginUser);
-        try{
-            await userStore.login(noLogin);
-        }catch(error){
-            console.log('에러 =>',error);
-        }
-    };
+// id, password 입력 확인 => id, password 입력시 버튼 활성화
+const disableSubmit = computed(
+  () => !(loginUser.userId && loginUser.password)
+);
 
-    /////라우터 이동
-    //회원가입 이동
-    const GoSignup = () => {
-            router.push('./user/signup')
-        };
-    //아이디 찾기 이동
-    const GoFindId = () => {
-            router.push('./user/findid')
-        };
-    //비밀번호 찾기 이동
-    const GoFindPwd = ()=>{
-            router.push('./user/findpwd')
-        };
+//로그인
+const login = async () => {
+  console.log(loginUser);
+  try {
+    await userStore.login(loginUser);
+    router.push('/home');
+  } catch (error) {
+    console.log('에러 =>', error.message); // 에러 로그 출력
+    alert(error.message); // 경고창으로 에러 메시지 표시
+  }
+};
 
-    </script>
-    
-    <style scoped>
-        .Page {
-            width: 100%;
-            min-width: 340px;  /*스마트폰 기준 너비 */
-            margin: 0 auto;
-            padding: 20px;
-        }
+//가입없이 이용
+const unsignLogin = async () => {
+  console.log(loginUser);
+  try {
+    await userStore.login(noLogin);
+  } catch (error) {
+    console.log('에러 =>', error);
+  }
+};
 
-        ul{
-            list-style-type: none;
+/////라우터 이동
+//회원가입 이동
+const GoSignup = () => {
+  router.push('./user/signup');
+};
+//아이디 찾기 이동
+const GoFindId = () => {
+  router.push('./user/findid');
+};
+//비밀번호 찾기 이동
+const GoFindPwd = () => {
+  router.push('./user/findpwd');
+};
 
-        }
-    </style>
+//소셜 회원가입 페이지 이동
+const GoSocialSignup = () => {
+  router.push('/user/social-signup'); // 소셜 회원가입 페이지 경로
+};
+</script>
+
+<style scoped>
+.Page {
+  width: 100%;
+  min-width: 340px; /*스마트폰 기준 너비 */
+  margin: 0 auto;
+  padding: 20px;
+}
+
+ul {
+  list-style-type: none;
+}
+</style>
