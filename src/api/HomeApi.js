0637b2cad768fc9814eef74e33a88cd87ad8299c
Yongjooon <95464174+Yongjooon@users.noreply.github.com>
2024-10-10 19:55:46 +0900

    [BEBIG-256] feat : bankLogin-edit
    자산 불러오기 수정
    자산 불러올때 로딩 글씨 생성

--- src/api/HomeApi.js
+++ src/api/HomeApi.js
@@ -54,7 +54,7 @@ export default {
     // 은행 아이디, 비번 받고, 해당 은행 계좌 정보 받아오기
     async getAccountList(bankAccount) {
         const { data } = await api.post(
-          `${BASE_URL}/account`,
+          `${BASE_URL}/account/add`,
           bankAccount,
           { 
             headers: {
@@ -63,20 +63,6 @@ export default {
           });
         return data;
     },
-    //확인된 계좌 정보들 db에 저장하기
-    async addAccount(accountList){
-     // console.log(accountList);
-      const { data } = await api.post(
-        `${BASE_URL}/account/add`,
-        accountList,
-        { 
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-      return data;
-    },
-
 
   // 미션보기
   async missionList(userId) {
