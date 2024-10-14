15aa0271ad82b521e4f9e29adbae79973617c2bc
Yongjooon <95464174+Yongjooon@users.noreply.github.com>
2024-10-14 17:46:55 +0900

    [div] fix : header 갱신

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <Loading />
   <div class="flex flex-col relative">
     <Header v-if="!isUserPage" class="lg:hidden"/>
-    <WebHeader v-if="!isUserPage" class="hidden lg:flex w-full mx-auto" />
+    <WebHeader v-if="!isUserPage" :key="footerStore.footerKey" class="hidden lg:flex w-full mx-auto" />
     <div :class="isUserPage ? 'flex-1 mt-0' : 'flex-1 mt-[50px] lg:mt-[50px] mb-2 overflow-y-auto'">
       <RouterView/>
     </div>
-    <Footer v-if="!isUserPage" class="lg:hidden"/>
+    <Footer v-if="!isUserPage" :key="footerStore.footerKey" class="lg:hidden"/>
   </div>
 </template>
 
@@ -18,14 +18,19 @@ import Loading from "./components/Loading.vue";
 
 import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import { useFooterStore } from '@/stores/footerStore';
 
 const route = useRoute();
+const footerStore = useFooterStore();
 const isUserPage = ref(route.path.startsWith('/user'));
+
 
 // 경로가 변경될 때마다 isUserPage 업데이트
 watch(route, (newRoute) => {
   isUserPage.value = newRoute.path.startsWith('/user');
 });
+
+
 
 </script>
 
